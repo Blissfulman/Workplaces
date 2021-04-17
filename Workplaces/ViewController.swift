47cb91b4ac1fe8c9922987651bc9533c149cc82b
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-17 22:56:55 +0500

    Initial Commit

---
+++ Workplaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
