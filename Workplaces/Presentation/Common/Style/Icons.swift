743132299b9129997c2cb60e3bc90d6223ba16df
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-18 22:45:07 +0500

    Добавлены иконки и соответствующие им константы

---
+++ Workplaces/Presentation/Common/Style/Icons.swift
@@ -0,0 +1,36 @@
+//
+//  Icons.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 18.04.2021.
+//
+
+import UIKit
+
+enum Icons {
+    static let geo = #imageLiteral(resourceName: "36-geo")
+    static let image = #imageLiteral(resourceName: "36-img")
+    static let send = #imageLiteral(resourceName: "36-send")
+    static let trash = #imageLiteral(resourceName: "36-trash")
+    static let edit = #imageLiteral(resourceName: "32-edit")
+    static let back = #imageLiteral(resourceName: "24-back")
+    static let backSmall = #imageLiteral(resourceName: "32-back")
+    static let close = #imageLiteral(resourceName: "24-close")
+    static let crossSmall = #imageLiteral(resourceName: "24-cross_small")
+    static let calendar = #imageLiteral(resourceName: "24-calendar")
+    
+    static let home = #imageLiteral(resourceName: "home")
+    static let homeActive = #imageLiteral(resourceName: "home_active")
+    static let newPost = #imageLiteral(resourceName: "new_post")
+    static let newPostActive = #imageLiteral(resourceName: "new_post_active")
+    static let profile = #imageLiteral(resourceName: "profile")
+    static let profileActive = #imageLiteral(resourceName: "profile_active")
+    static let like = #imageLiteral(resourceName: "like")
+    static let liked = #imageLiteral(resourceName: "liked")
+    
+    static let googleButton = #imageLiteral(resourceName: "google_button")
+    static let facebookButton = #imageLiteral(resourceName: "facebook_button")
+    static let vkButton = #imageLiteral(resourceName: "vk_button")
+    
+    static let avatar = #imageLiteral(resourceName: "avatar")
+}
