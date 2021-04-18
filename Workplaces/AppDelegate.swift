fae9fd9945b3150f63dde316340353e3b5ee0b15
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-18 10:12:47 +0500

    Включены правила SwiftLint, небольшой рефакторинг

--- Workplaces/AppDelegate.swift
+++ Workplaces/AppDelegate.swift
@@ -9,14 +9,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
+    // MARK: - UISceneSession Lifecycle
+    
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
