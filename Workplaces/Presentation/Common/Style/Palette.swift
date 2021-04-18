3927cb3153105892bcbfeaab5aa4b54ee0b62a7d
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-18 15:45:27 +0500

    Добавлены набор цветов и константы цветов

---
+++ Workplaces/Presentation/Common/Style/Palette.swift
@@ -0,0 +1,17 @@
+//
+//  Palette.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 18.04.2021.
+//
+
+import UIKit
+
+enum Palette {
+    static let orange = UIColor(named: "orange")
+    static let darkGrey = UIColor(named: "darkGrey")
+    static let middleGrey = UIColor(named: "middleGrey")
+    static let grey = UIColor(named: "grey")
+    static let lightGrey = UIColor(named: "lightGrey")
+    static let lightGreyBlue = UIColor(named: "lightGreyBlue")
+}
