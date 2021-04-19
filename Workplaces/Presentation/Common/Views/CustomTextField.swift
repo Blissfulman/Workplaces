9ee49788ec006706baca21d99daa613656bf6c8c
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-19 22:54:12 +0500

    Небольшое обновление структуры проекта

---
+++ Workplaces/Presentation/Common/Views/CustomTextField.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTextField.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 19.04.2021.
+//
+
+import UIKit
+
+// final class CustomTextField: UITextField {
+//    
+//    func clearButtonWithImage(_ image: UIImage) {
+//        let clearButton = UIButton()
+//        clearButton.setImage(image, for: .normal)
+//        clearButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+//        clearButton.contentMode = .scaleAspectFit
+//        rightView = clearButton
+//        rightViewMode = .whileEditing
+//    }
+//    
+//    override func draw(_ rect: CGRect) {
+//        super.draw(rect)
+//        clearButtonWithImage(Icons.crossSmall)
+//    }
+// }
