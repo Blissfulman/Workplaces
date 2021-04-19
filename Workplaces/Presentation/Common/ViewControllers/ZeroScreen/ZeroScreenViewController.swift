0a86fa41045d1366c8bb48901f798ed9a6375475
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-19 10:25:15 +0500

    Добавлены экран с надписью "Упс..." и 2 шрифта

---
+++ Workplaces/Presentation/Common/ViewControllers/ZeroScreen/ZeroScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ZeroScreenViewController.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 18.04.2021.
+//
+
+import UIKit
+
+class ZeroScreenViewController: UIViewController {
+    
+    // MARK: - Lifecycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupUI() {
+        
+    }
+}
