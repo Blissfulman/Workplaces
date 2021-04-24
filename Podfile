f5530677de01850ff8060ab8b2d48c7b2b25f2c6
Evgeny Novgorodov <greatly01@ya.ru>
2021-04-25 00:37:29 +0500

    Добавлен таргет WorkplacesAPI

--- Podfile
+++ Podfile
@@ -5,6 +5,6 @@ target 'Workplaces' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-#  pod 
+  pod 'Apexy'
 
 end
---
+++ WorkplacesAPI/WorkplacesAPI.h
@@ -0,0 +1,18 @@
+//
+//  WorkplacesAPI.h
+//  WorkplacesAPI
+//
+//  Created by Evgeny Novgorodov on 25.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkplacesAPI.
+FOUNDATION_EXPORT double WorkplacesAPIVersionNumber;
+
+//! Project version string for WorkplacesAPI.
+FOUNDATION_EXPORT const unsigned char WorkplacesAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkplacesAPI/PublicHeader.h>
+
+
