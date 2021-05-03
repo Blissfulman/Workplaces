5e566da9c8139f971c0be7315675072ca2927327
Evgeny Novgorodov <greatly01@ya.ru>
2021-05-03 22:08:11 +0500

    Добавлены тесты декодирования в LoginEndpoint и RefreshTokenEndpoint

--- WorkplacesAPITests/Endpoint/Authorization/LoginEndpointTests.swift
+++ WorkplacesAPITests/Endpoint/Authorization/LoginEndpointTests.swift
@@ -19,4 +19,31 @@ final class LoginEndpointTests: XCTestCase {
         assertURL(urlRequest, "auth/login")
         assertJsonBody(urlRequest, ["email": "email", "password": "password"])
     }
+    
+    func testContentDecoding() throws {
+        let jsonData = """
+            {
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM",
+                "refresh_token": "8feed535-5ca5-464e-862d-0de124800aa3"
+            }
+            """.data(using: .utf8)!
+        
+        let userCredentials = UserCredentials(email: "test", password: "test")
+        let endpoint = LoginEndpoint(userCredentials: userCredentials)
+        
+        XCTAssertNotNil(try endpoint.content(from: nil, with: jsonData), "Error decoding")
+        
+        let authorizationData = try endpoint.content(from: nil, with: jsonData)
+        
+        XCTAssertEqual(
+            authorizationData.accessToken,
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM",
+            "Error access token decoding"
+        )
+        XCTAssertEqual(
+            authorizationData.refreshToken,
+            "8feed535-5ca5-464e-862d-0de124800aa3",
+            "Error refresh token decoding"
+        )
+    }
 }
