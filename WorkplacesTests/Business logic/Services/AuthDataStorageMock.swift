80d370e97f773b13830bf15d5b126c94c3840e79
Evgeny Novgorodov <greatly01@ya.ru>
2021-05-04 13:51:09 +0500

    Добавлен тест сохранения токенов в хранилище при авторизации

---
+++ WorkplacesTests/Business logic/Services/AuthDataStorageMock.swift
@@ -0,0 +1,53 @@
+//
+//  AuthDataStorageMock.swift
+//  WorkplacesTests
+//
+//  Created by Evgeny Novgorodov on 03.05.2021.
+//
+
+@testable import Workplaces
+import WorkplacesAPI
+import XCTest
+
+final class AuthDataStorageMock: AuthDataStorage {
+    
+    // MARK: - Public properties
+    
+    public var isRefreshingToken = false
+    
+    public var accessToken: String? {
+        storage.get(forKey: accessTokenKey)
+    }
+    
+    public var refreshToken: String? {
+        storage.get(forKey: refreshTokenKey)
+    }
+    
+    // MARK: - Private properties
+    
+    private let storage: StringStorage
+    private let accessTokenKey = "AccessTokenTest"
+    private let refreshTokenKey = "RefreshTokenTest"
+    
+    // MARK: - Initializers
+    
+    public init(storage: StringStorage) {
+        self.storage = storage
+    }
+    
+    // MARK: - Public methods
+    
+    public func saveAuthData(_ data: AuthorizationData) {
+        storage.save(data.accessToken, forKey: accessTokenKey)
+        storage.save(data.refreshToken, forKey: refreshTokenKey)
+    }
+    
+    public func deleteAuthData() {
+        storage.remove(forKey: accessTokenKey)
+        storage.remove(forKey: refreshTokenKey)
+    }
+    
+    public func set(isRefreshingToken: Bool) {
+        self.isRefreshingToken = isRefreshingToken
+    }
+}
