d850a19e5ef74f1af70b1a7f7d9cc6f16f25add6
Evgeny Novgorodov <greatly01@ya.ru>
2021-05-17 16:20:56 +0500

    Локализован ZeroViewModel

--- Workplaces/Presentation/Common/Views/Zero/ZeroViewModel.swift
+++ Workplaces/Presentation/Common/Views/Zero/ZeroViewModel.swift
@@ -25,32 +25,32 @@ extension ZeroViewModel {
     
     static let error = ZeroViewModel(
         image: Images.errorScreen,
-        title: "Упс...",
-        subtitle: "Что-то пошло не так",
-        buttonTitle: "Обновить"
+        title: "Oops...".localized(),
+        subtitle: "Something went wrong",
+        buttonTitle: "Refresh".localized()
     )
     static let feedNoFriends = ZeroViewModel(
         image: Images.noDataScreen,
-        title: "Пустота",
-        subtitle: "Вам нужны друзья, чтобы лента стала живой",
-        buttonTitle: "Найти друзей"
+        title: "Empty".localized(),
+        subtitle: "You need friends to the feed became alive".localized(),
+        buttonTitle: "Find friends".localized()
     )
     static let profileNoPosts = ZeroViewModel(
         image: Images.noDataScreen,
-        title: "Пустота",
-        subtitle: "Если молчать, люди никогда не узнают о вас",
-        buttonTitle: "Создать пост"
+        title: "Empty".localized(),
+        subtitle: "If you remain silent, people will never know about you".localized(),
+        buttonTitle: "Create a post".localized()
     )
     static let profileNoLikes = ZeroViewModel(
         image: Images.noDataScreen,
-        title: "Пустота",
-        subtitle: "Вы ещё не поставили ни одного лайка, но можете из ленты",
-        buttonTitle: "Перейти к ленте"
+        title: "Empty".localized(),
+        subtitle: "You haven’t put any likes yet, but you can do it from the feed".localized(),
+        buttonTitle: "Go to the feed".localized()
     )
     static let profileNoFriends = ZeroViewModel(
         image: Images.noDataScreen,
-        title: "Пустота",
-        subtitle: "Вы пока одиноки в сервисе, но это можно исправить",
-        buttonTitle: "Найти друзей"
+        title: "Empty".localized(),
+        subtitle: "You are still alone in the service, but this can be fixed".localized(),
+        buttonTitle: "Find friends".localized()
     )
 }
