2c98c05689db47d7bbfc9060a8718e72b48bd963
Evgeny Novgorodov <greatly01@ya.ru>
2021-05-27 14:36:51 +0500

    Обновлены MainFilledButton и MainUnfilledButton

--- Workplaces/Presentation/Common/Views/MainFilledButton.swift
+++ Workplaces/Presentation/Common/Views/MainFilledButton.swift
@@ -9,17 +9,24 @@ import UIKit
 
 final class MainFilledButton: UIButton {
     
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
     // MARK: - UIView
     
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundColor = isEnabled ? Palette.orange : Palette.lightGreyBlue
         titleLabel?.textColor = isEnabled ? Palette.white : Palette.middleGrey
-    }
-    
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        setCornerRadius(UIConstants.buttonCornerRadius)
     }
     
     // MARK: - UIButton
@@ -29,5 +36,13 @@ final class MainFilledButton: UIButton {
             guard oldValue != isHighlighted else { return }
             isHighlighted ? downscaleAnimation() : resetScaleAnimation()
         }
+    }
+    
+    // MARK: - Private methods
+    
+    private func commonInit() {
+        backgroundColor = .clear
+        isExclusiveTouch = true
+        setCornerRadius(UIConstants.buttonCornerRadius)
     }
 }
--- Workplaces/Presentation/Common/Views/MainUnfilledButton.swift
+++ Workplaces/Presentation/Common/Views/MainUnfilledButton.swift
@@ -9,11 +9,29 @@ import UIKit
 
 final class MainUnfilledButton: UIButton {
     
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
     // MARK: - UIView
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundColor = .clear
         titleLabel?.textColor = isEnabled ? Palette.orange : Palette.middleGrey
+    }
+    
+    // MARK: - Private methods
+    
+    private func commonInit() {
+        backgroundColor = .clear
+        isExclusiveTouch = true
     }
 }
