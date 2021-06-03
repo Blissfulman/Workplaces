780bfa05d7a271c37d56f658d179ae9c895e6ff6
Evgeny Novgorodov <greatly01@ya.ru>
2021-06-03 23:25:17 +0500

    Добавлена основа PinCodeViewController

---
+++ Workplaces/Presentation/Common/ViewControllers/PinCode/PinCodeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PinCodeViewController.swift
+//  Workplaces
+//
+//  Created by Evgeny Novgorodov on 03.06.2021.
+//
+
+import UIKit
+
+final class PinCodeViewController: BaseViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private var pinCodeTextField: UITextField!
+    
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func numberButtonTapped(_ sender: UIButton) {
+        if let buttonNumber = sender.titleLabel?.text {
+            pinCodeTextField.text = "\(pinCodeTextField.text ?? "")\(buttonNumber)"
+        }
+        print(pinCodeTextField.text)
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupUI() {
+        pinCodeTextField.font = Fonts.title
+    }
+}
